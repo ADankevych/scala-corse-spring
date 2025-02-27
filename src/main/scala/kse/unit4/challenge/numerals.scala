d6f7fc029bba26d923e5281fe76ea483fd7c3d21
adankevych <adankevych@kse.org.ua>
2025-02-27 15:16:27 +0200

    unit-4

--- src/main/scala/kse/unit4/challenge/numerals.scala
+++ src/main/scala/kse/unit4/challenge/numerals.scala
@@ -10,76 +10,92 @@ object numerals:
 
     def predecessor: Numeral
 
-    def successor: Numeral = ???
+    def successor: Numeral = Successor(this)
 
     @targetName("greater than")
-    infix def >(that: Numeral): Boolean
+    infix def >(that: Numeral): Boolean = this.toInt > that.toInt
 
     @targetName("greater or equal to")
-    infix def >=(that: Numeral): Boolean = ???
+    infix def >=(that: Numeral): Boolean = this.toInt >= that.toInt
 
     @targetName("less than")
-    infix def <(that: Numeral): Boolean = ???
+    infix def <(that: Numeral): Boolean = this.toInt < that.toInt
 
     @targetName("less or equal to")
-    infix def <=(that: Numeral): Boolean = ???
+    infix def <=(that: Numeral): Boolean = this.toInt <= that.toInt
 
     @targetName("addition")
-    infix def +(that: Numeral): Numeral
+    infix def +(that: Numeral): Numeral =
+      numerals.Numeral.fromInt(this.toInt + that.toInt)
 
     // Optional
     @targetName("subtraction")
-    infix def -(that: Numeral): Numeral
+    infix def -(that: Numeral): Numeral =
+      numerals.Numeral.fromInt(math.max(0, this.toInt - that.toInt))
 
     def toInt: Int
 
     override def toString: String = s"Nat($predecessor)"
 
+  object Numeral:
+
+    def fromInt(n: Int): Numeral =
+      if n <= 0 then Zero
+      else Successor(fromInt(n - 1))
+
   type Zero = Zero.type
 
   object Zero extends Numeral:
 
-    def isZero: Boolean = ???
+    def isZero: Boolean = true
 
-    def predecessor: Numeral = ???
+    def predecessor: Numeral = throw new UnsupportedOperationException("Zero has no predecessor")
 
     @targetName("greater than")
-    infix def >(that: Numeral): Boolean = ???
+    override infix def >(that: Numeral): Boolean = this.toInt > that.toInt
 
     @targetName("addition")
-    infix def +(that: Numeral): Numeral = ???
+    override infix def +(that: Numeral): Numeral =
+      numerals.Numeral.fromInt(this.toInt + that.toInt)
 
     // Optional
     @targetName("subtraction")
-    infix def -(that: Numeral): Numeral = ???
+    override infix def -(that: Numeral): Numeral =
+      numerals.Numeral.fromInt(math.max(0, this.toInt - that.toInt))
 
-    def toInt: Int = ???
+    def toInt: Int = 0
 
-    override def toString: String = ???
+    override def toString: String = "Zero"
 
-    override def equals(obj: Any): Boolean = ???
+    override def equals(obj: Any): Boolean = obj match
+      case n: Numeral => this.toInt == n.toInt
+      case _          => false
 
   object Successor:
     def unapply(successor: Successor): Option[Numeral] = Option(successor.predecessor)
 
   class Successor(n: Numeral) extends Numeral:
 
-    def isZero: Boolean = ???
+    def isZero: Boolean = false
 
-    def predecessor: Numeral = ???
+    def predecessor: Numeral = n
 
     @targetName("greater than")
-    infix def >(that: Numeral): Boolean = ???
+    override infix def >(that: Numeral): Boolean = this.toInt > that.toInt
 
     @targetName("addition")
-    infix def +(that: Numeral): Numeral = ???
+    override infix def +(that: Numeral): Numeral =
+      numerals.Numeral.fromInt(this.toInt + that.toInt)
 
     // Optional
     @targetName("subtraction")
-    infix def -(that: Numeral): Numeral = ???
+    override infix def -(that: Numeral): Numeral =
+      numerals.Numeral.fromInt(math.max(0, this.toInt - that.toInt))
 
     def toInt: Int = 1 + n.toInt
 
-    override def toString: String = ???
+    override def toString: String = s"Succ(${n.toInt})"
 
-    override def equals(obj: Any): Boolean = ???
+    override def equals(obj: Any): Boolean = obj match
+      case n: Numeral => this.toInt == n.toInt
+      case _          => false
--- src/test/scala/kse/unit4/challenge/NumeralsSpecification.scala
+++ src/test/scala/kse/unit4/challenge/NumeralsSpecification.scala
@@ -11,4 +11,37 @@ object NumeralsSpecification extends Properties("Numerals"):
   override def overrideParameters(p: Parameters): Parameters =
     p.withMinSuccessfulTests(50).withMaxDiscardRatio(100)
 
+  property("Zero is zero") = forAll: (n: Zero) =>
+    n.toInt == 0 && n.isZero
+
+  property("Successor increases value by 1") = forAll: (n: Numeral) =>
+    n.successor.toInt == n.toInt + 1
+
+  property("Predecessor decreases value by 1 unless Zero") = forAll: (n: Successor) =>
+    n.predecessor.toInt == n.toInt - 1
+
+  property("Addition is commutative") = forAll: (a: Numeral, b: Numeral) =>
+    (a + b).toInt == (b + a).toInt
+
+  property("Addition is associative") = forAll: (a: Numeral, b: Numeral, c: Numeral) =>
+    ((a + b) + c).toInt == (a + (b + c)).toInt
+
+  property("Subtraction from itself results in Zero") = forAll: (a: Numeral) =>
+    (a - a) == Zero
+
+  property("Subtraction is inverse of addition for successor values") = forAll: (a: Numeral, b: Numeral) =>
+    (a + b) - b == a
+
+  property("Zero is the identity for addition") = forAll: (a: Numeral) =>
+    (a + Zero) == a && (Zero + a) == a
+
+  property("Greater than and less than are consistent") = forAll: (a: Numeral, b: Numeral) =>
+    (a > b) == (b < a)
+
+  property("Greater or equal is consistent with less or equal") = forAll: (a: Numeral, b: Numeral) =>
+    (a >= b) == !(a < b)
+
+  property("Zero has no predecessor") =
+    throws(classOf[UnsupportedOperationException]) { Zero.predecessor }
+
 end NumeralsSpecification
