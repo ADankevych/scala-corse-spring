765ea964978715c2ac53310439885d426bae3e5d
adankevych <adankevych@kse.org.ua>
2025-03-15 15:11:51 +0200

    unit-5

--- src/main/scala/kse/unit5/challenge/set.scala
+++ src/main/scala/kse/unit5/challenge/set.scala
@@ -1,7 +1,6 @@
 package kse.unit5.challenge
 
 import kse.unit4.challenge.numerals.Numeral
-
 import scala.annotation.targetName
 
 object set:
@@ -16,9 +15,7 @@ object set:
 
     infix def include(x: Numeral): NumeralSet
 
-    // Optional
-    // Uncomment if needed
-//    infix def remove(x: Numeral): NumeralSet
+    infix def remove(x: Numeral): NumeralSet
 
     @targetName("union")
     infix def ∪(that: NumeralSet): NumeralSet
@@ -26,15 +23,11 @@ object set:
     @targetName("intersection")
     infix def ∩(that: NumeralSet): NumeralSet
 
-  // Optional
-  // Uncomment if needed
-//    @targetName("difference")
-//    infix def \(that: NumeralSet): NumeralSet
+    @targetName("difference")
+    infix def \(that: NumeralSet): NumeralSet
 
-  // Optional
-  // Uncomment if needed
-//    @targetName("symmetric difference")
-//    infix def ∆(that: NumeralSet): NumeralSet = ???
+    @targetName("symmetric difference")
+    infix def ∆(that: NumeralSet): NumeralSet
 
   end NumeralSet
 
@@ -42,63 +35,89 @@ object set:
 
   case object Empty extends NumeralSet:
 
-    infix def forAll(predicate: Numeral => Boolean): Boolean = ???
+    infix def forAll(predicate: Numeral => Boolean): Boolean = true
 
-    infix def exists(predicate: Numeral => Boolean): Boolean = ???
+    infix def exists(predicate: Numeral => Boolean): Boolean = false
 
-    infix def contains(x: Numeral): Boolean = ???
+    infix def contains(x: Numeral): Boolean = false
 
-    infix def include(x: Numeral): NumeralSet = ???
+    infix def include(x: Numeral): NumeralSet = NonEmpty(Empty, x, Empty)
 
-    // Optional
-    // Uncomment if needed
-//    infix def remove(x: Numeral): NumeralSet = ???
+    infix def remove(x: Numeral): NumeralSet = this
 
     @targetName("union")
-    infix def ∪(that: NumeralSet): NumeralSet = ???
+    infix def ∪(that: NumeralSet): NumeralSet = that
 
     @targetName("intersection")
-    infix def ∩(that: NumeralSet): NumeralSet = ???
+    infix def ∩(that: NumeralSet): NumeralSet = Empty
 
-    // Optional
-    // Uncomment if needed
-//    @targetName("difference")
-//    infix def \(that: NumeralSet): NumeralSet = ???
+    @targetName("difference")
+    infix def \(that: NumeralSet): NumeralSet = Empty
+
+    @targetName("symmetric difference")
+    infix def ∆(that: NumeralSet): NumeralSet = that
 
     override def toString: String = "[*]"
 
-    override def equals(obj: Any): Boolean = ???
+    override def equals(obj: Any): Boolean = obj.isInstanceOf[Empty]
 
   end Empty
 
   case class NonEmpty(left: NumeralSet, element: Numeral, right: NumeralSet) extends NumeralSet:
 
-    infix def forAll(predicate: Numeral => Boolean): Boolean = ???
+    infix def forAll(predicate: Numeral => Boolean): Boolean =
+      left.forAll(predicate) && predicate(element) && right.forAll(predicate)
 
-    infix def exists(predicate: Numeral => Boolean): Boolean = ???
+    infix def exists(predicate: Numeral => Boolean): Boolean =
+      left.exists(predicate) || predicate(element) || right.exists(predicate)
 
-    infix def contains(x: Numeral): Boolean = ???
+    infix def contains(x: Numeral): Boolean =
+      left.contains(x) || x == element || right.contains(x)
 
-    infix def include(x: Numeral): NumeralSet = ???
+    infix def include(x: Numeral): NumeralSet =
+      if x < element then NonEmpty(left.include(x), element, right)
+      else if x > element then NonEmpty(left, element, right.include(x))
+      else this
 
-    // Optional
-    // Uncomment if needed
-//    infix def remove(x: Numeral): NumeralSet = ???
+    infix def remove(x: Numeral): NumeralSet =
+      if x < element then NonEmpty(left.remove(x), element, right)
+      else if x > element then NonEmpty(left, element, right.remove(x))
+      else {
+        val union = left ∪ right
+        if union.contains(x) then union.remove(x) else union
+      }
 
     @targetName("union")
-    infix def ∪(that: NumeralSet): NumeralSet = ???
+    infix def ∪(that: NumeralSet): NumeralSet =
+      that match
+        case Empty => this
+        case NonEmpty(l, e, r) =>
+          if e < element then NonEmpty(l ∪ left, e, right ∪ r)
+          else if e > element then NonEmpty(left ∪ l, element, right ∪ r)
+          else NonEmpty(left ∪ l, element, right ∪ r)
 
     @targetName("intersection")
-    infix def ∩(that: NumeralSet): NumeralSet = ???
+    infix def ∩(that: NumeralSet): NumeralSet =
+      (left ∩ that) ∪ (right ∩ that) match
+        case Empty       => Empty
+        case nonEmptySet => if that.contains(element) then NonEmpty(nonEmptySet, element, Empty) else nonEmptySet
 
-    // Optional
-    // Uncomment if needed
-//    @targetName("difference")
-//    infix def \(that: NumeralSet): NumeralSet = ???
+    @targetName("difference")
+    infix def \(that: NumeralSet): NumeralSet =
+      if that.contains(element) then (left \ that) ∪ (right \ that)
+      else NonEmpty(left \ that, element, right \ that)
+
+    @targetName("symmetric difference")
+    infix def ∆(that: NumeralSet): NumeralSet =
+      (this ∪ that) \ (this ∩ that)
 
     override def toString: String = s"[$left - [$element] - $right]"
 
-    override def equals(obj: Any): Boolean = ???
+    override def equals(obj: Any): Boolean =
+      obj match
+        case Empty => false
+        case NonEmpty(l, e, r) =>
+          left == l && element == e && right == r
 
   end NonEmpty
 
