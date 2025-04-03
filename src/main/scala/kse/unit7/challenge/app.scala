7149735eb8e8c0168b7bf7a6eee4da2529960a39
adankevych <adankevych@kse.org.ua>
2025-04-04 01:33:30 +0300

    unit-7

--- src/main/scala/kse/unit7/challenge/app.scala
+++ src/main/scala/kse/unit7/challenge/app.scala
@@ -6,10 +6,39 @@ import kse.unit7.challenge.services.*
 
 object app:
 
-  def getPostsViews(apiKey: ApiKey): Try[List[PostView]] = ???
+  def getPostsViews(apiKey: ApiKey): Try[List[PostView]] =
+    services.getUserProfile(apiKey).flatMap { userProfile =>
+      services.getPosts(userProfile.userId).flatMap { posts =>
+        posts
+          .foldLeft(Try(List.empty[PostView])) { (acc, post) =>
+            for {
+              list <- acc
+              view <- getPostView(post)
+            } yield view :: list
+          }
+          .map(_.reverse)
+      }
+    }
 
-  def getPostsViewDesugared(apiKey: ApiKey): Try[List[PostView]] = ???
+  def getPostsViewDesugared(apiKey: ApiKey): Try[List[PostView]] =
+    services
+      .getUserProfile(apiKey)
+      .flatMap(userProfile =>
+        services
+          .getPosts(userProfile.userId)
+          .flatMap(posts => posts.foldLeft(Try(List.empty[PostView]))((acc, post) => acc.flatMap(list => getPostView(post).map(_ :: list))).map(_.reverse))
+      )
 
-  def getPostView(post: Post): Try[PostView] = ???
+  def getPostView(post: Post): Try[PostView] =
+    for {
+      comments <- services.getComments(post.postId)
+      likes    <- services.getLikes(post.postId)
+      shares   <- services.getShares(post.postId)
+    } yield PostView(post, comments, likes, shares)
 
-  def getPostViewDesugared(post: Post): Try[PostView] = ???
+  def getPostViewDesugared(post: Post): Try[PostView] =
+    services
+      .getComments(post.postId)
+      .flatMap(comments =>
+        services.getLikes(post.postId).flatMap(likes => services.getShares(post.postId).map(shares => PostView(post, comments, likes, shares)))
+      )
