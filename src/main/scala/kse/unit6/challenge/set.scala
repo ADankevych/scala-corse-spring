cf8d3bb8b8e715581d11230eb56a7000135af9f4
Anastasiia Dankevych <150438964+ADankevych@users.noreply.github.com>
2025-04-14 11:49:56 +0300

    Unit 6 (#6)
    * unit-4
    
    * unit-4 scalafmt
    
    * unit-4 fixed, I hope
    
    * unit-5
    
    * ci file
    
    * unit-5 fixed tests
    
    * scalafmt
    
    * scalafmt
    
    * scalafmt
    
    * scalafmt, empty files for lint
    
    * scalafmt, empty files for lint
    
    * scalafmt, unit-6 for lint
    
    * scalafmt, unit-6 deleted
    
    * unit-6
    
    * unit-6
    
    * unit-6
    
    * unit-6 scalafmt
    
    * unit-5 hashcode
    
    * hashcode

--- src/main/scala/kse/unit6/challenge/set.scala
+++ src/main/scala/kse/unit6/challenge/set.scala
@@ -15,23 +15,13 @@ object set:
 
     infix def include[B >: A: Order](x: B): Set[B]
 
-    // Optional from the Unit 5. If you haven't implement it in Unit 5 then skip it
-    //    infix def remove[B >: A: Order](x: B): Set[B]
-
     @targetName("union")
     infix def ∪[B >: A: Order](that: Set[B]): Set[B]
 
     @targetName("intersection")
     infix def ∩[B >: A: Order](that: Set[B]): Set[B]
 
-    // Optional from the Unit 5. If you haven't implement it in Unit 5 then skip it
-    //    @targetName("difference")
-    //    infix def \[B >: A: Order](that: Set[B]): Set[B]
-
-    // Optional from the Unit 5. If you haven't implement it in Unit 5 then skip it
-    //    @targetName("symmetric difference")
-    //    infix def ∆[B >: A: Order](that: Set[B]): Set[B] = ???
-
+    def isSubsetOf(that: Set[?]): Boolean = this.forAll(element => that.exists(_ == element))
   end Set
 
   type Empty = Empty.type
@@ -39,58 +29,72 @@ object set:
   // TODO: Remind about type system
   case object Empty extends Set[Nothing]:
 
-    infix def forAll(predicate: Nothing => Boolean): Boolean = ???
+    infix def forAll(predicate: Nothing => Boolean): Boolean = true
 
-    infix def exists(predicate: Nothing => Boolean): Boolean = ???
+    infix def exists(predicate: Nothing => Boolean): Boolean = false
 
-    infix def contains[B: Order](x: B): Boolean = ???
+    infix def contains[B: Order](x: B): Boolean = false
 
-    infix def include[B: Order](x: B): Set[B] = ???
-
-    // Optional from the Unit 5. If you haven't implement it in Unit 5 then skip it
-    //    infix def remove[B: Order](x: B): Set[B] = ???
+    infix def include[B: Order](x: B): Set[B] = NonEmpty(Empty, x, Empty)
 
     @targetName("union")
-    infix def ∪[B: Order](that: Set[B]): Set[B] = ???
+    infix def ∪[B: Order](that: Set[B]): Set[B] = that
 
     @targetName("intersection")
-    infix def ∩[B: Order](that: Set[B]): Set[B] = ???
-
-    // Optional from the Unit 5. If you haven't implement it in Unit 5 then skip it
-    //    @targetName("difference")
-    //    infix def \[B: Order](that: Set[B]): Set[B] = ???
+    infix def ∩[B: Order](that: Set[B]): Set[B] = Empty
 
     override def toString: String = "[*]"
 
-    override def equals(obj: Any): Boolean = ???
+    override def equals(obj: Any): Boolean = obj match
+      case _: Empty.type => true
+      case _             => false
 
+    override def hashCode(): Int = 0
   end Empty
 
   case class NonEmpty[A](left: Set[A], element: A, right: Set[A]) extends Set[A]:
 
-    infix def forAll(predicate: A => Boolean): Boolean = ???
+    infix def forAll(predicate: A => Boolean): Boolean =
+      predicate(element) && left.forAll(predicate) && right.forAll(predicate)
 
-    infix def exists(predicate: A => Boolean): Boolean = ???
+    infix def exists(predicate: A => Boolean): Boolean =
+      predicate(element) || left.exists(predicate) || right.exists(predicate)
 
-    infix def contains[B >: A: Order](x: B): Boolean = ???
+    infix def contains[B >: A: Order](x: B): Boolean =
+      val comparison = summon[Order[B]].compare(x, element)
+      if comparison == 0 then true
+      else if comparison < 0 then left.contains(x)
+      else right.contains(x)
 
-    infix def include[B >: A: Order](x: B): Set[B] = ???
-
-    // Optional from the Unit 5. If you haven't implement it in Unit 5 then skip it
-    //    infix def remove[B >: A: Order](x: B): Set[B] = ???
+    infix def include[B >: A: Order](x: B): Set[B] =
+      val comparison = summon[Order[B]].compare(x, element)
+      if comparison == 0 then this
+      else if comparison < 0 then NonEmpty(left.include(x), element, right)
+      else NonEmpty(left, element, right.include(x))
 
     @targetName("union")
-    infix def ∪[B >: A: Order](that: Set[B]): Set[B] = ???
+    infix def ∪[B >: A: Order](that: Set[B]): Set[B] =
+      (left ∪ (right ∪ that)).include(element)
 
     @targetName("intersection")
-    infix def ∩[B >: A: Order](that: Set[B]): Set[B] = ???
-
-    // Optional from the Unit 5. If you haven't implement it in Unit 5 then skip it
-    //    @targetName("difference")
-    //    infix def \[B >: A: Order](that: Set[B]): Set[B] = ???
+    infix def ∩[B >: A: Order](that: Set[B]): Set[B] =
+      if that.contains(element) then
+        val leftIntersection  = left ∩ that
+        val rightIntersection = right ∩ that
+        NonEmpty(leftIntersection, element, rightIntersection)
+      else (left ∩ that) ∪ (right ∩ that)
 
     override def toString: String = s"[$left - [$element] - $right]"
 
-    override def equals(obj: Any): Boolean = ???
+    override def equals(obj: Any): Boolean =
+      obj match
+        case that: NonEmpty[_] => that.isSubsetOf(this) && this.isSubsetOf(that)
+        case _                 => false
 
+    override def hashCode(): Int =
+      val leftHash    = left.hashCode()
+      val rightHash   = right.hashCode()
+      val elementHash = element.hashCode()
+      leftHash ^ rightHash ^ elementHash
   end NonEmpty
+end set
--- src/test/scala/kse/unit6/challenge/SetSpecification.scala
+++ src/test/scala/kse/unit6/challenge/SetSpecification.scala
@@ -19,157 +19,109 @@ end GeneralSetSpecification
 
 object EmptySpecification extends Properties("Empty set laws"):
 
-  property("Universal quantifier applied to the empty set should return true for any predicate") = ???
+  property("Universal quantifier applied to the empty set should return true for any predicate") = forAll: (numeral: Numeral) =>
+    Empty.forAll((_: Nothing) => false)
 
-  property("Existential quantifier applied to the empty set should return false for any predicate") = ???
+  property("Existential quantifier applied to the empty set should return false for any predicate") = forAll: (numeral: Numeral) =>
+    !Empty.exists((_: Nothing) => true)
 
   property("Empty set should not contain any element") = forAll: (numeral: Numeral) =>
-    ???
-
+    !Empty.contains(numeral)
 end EmptySpecification
 
 object NonEmptySpecification extends Properties("Non-empty set laws"):
 
   property("Universal quantifier applied to an non-empty set should return true for a tautology") = forAll: (nonEmpty: NonEmpty[Numeral]) =>
-    ???
+    nonEmpty.forAll((_: Numeral) => true)
 
   property("Universal quantifier applied to an non-empty set should return false for a contradiction") = forAll: (nonEmpty: NonEmpty[Numeral]) =>
-    ???
+    !nonEmpty.forAll((_: Numeral) => false)
 
   property("Existential quantifier applied to an non-empty set should return true for a tautology") = forAll: (nonEmpty: NonEmpty[Numeral]) =>
-    ???
+    nonEmpty.exists((_: Numeral) => true)
 
   property("Existential quantifier applied to an non-empty set should return false for a contradiction") = forAll: (nonEmpty: NonEmpty[Numeral]) =>
-    ???
+    !nonEmpty.exists((_: Numeral) => false)
 
   property("Single-element set should contain an element") = forAll: (numeral: Numeral) =>
-    ???
+    NonEmpty(Empty, numeral, Empty).contains(numeral)
 
 end NonEmptySpecification
 
 object SetSpecification extends Properties("Set laws"):
 
   property("Universal quantifier applied to a set should return true for a tautology") = forAll: (set: Set[Numeral]) =>
-    ???
+    set.forAll((_: Numeral) => true)
 
   property("Any set should contain its elements") = forAll: (set: Set[Numeral], numeral: Numeral) =>
-    ???
+    val NewSet = set.include(numeral)
+    NewSet.contains(numeral)
 
   property("If a set does not contain a given element then all elements of the set should not be equal to the given element") = forAll:
-    (set: Set[Numeral], numeral: Numeral) => ???
+    (set: Set[Numeral], numeral: Numeral) => !set.contains(numeral) ==> set.forAll(elem => !summon[Order[Numeral]].compare(elem, numeral).equals(0))
 
   property("If a set does not contain a given element then there is no element in the set equal to the given element") = forAll:
-    (set: Set[Numeral], numeral: Numeral) => ???
+    (set: Set[Numeral], numeral: Numeral) => !set.contains(numeral) ==> !set.exists(elem => summon[Order[Numeral]].compare(elem, numeral) == 0)
 
   property("If a given element is added to a set then there is an element in the set equals to the give element") = forAll:
-    (set: Set[Numeral], numeral: Numeral) => ???
+    (set: Set[Numeral], numeral: Numeral) =>
+      val NewSet = set.include(numeral)
+      NewSet.exists(elem => summon[Order[Numeral]].compare(elem, numeral) == 0)
 
   property("If a given element is added to a set then set should include the given element") = forAll: (set: Set[Numeral], numeral: Numeral) =>
-    ???
+    val NewSet = set.include(numeral)
+    NewSet.contains(numeral)
 
   property("Inclusion should be idempotent") = forAll: (set: Set[Numeral], numeral: Numeral) =>
-    ???
-
-  // Optional
-  // Uncomment if needed
-//  property("If a given element is removed from a set then all elements of the set should not be equals to the given element") = forAll:
-//    (set: Set[Numeral], numeral: Numeral) => ???
-
-  // Optional
-  // Uncomment if needed
-//  property("If a given element is removed from a set then the set should not contain the given element") = forAll: (set: Set[Numeral], numeral: Numeral) =>
-//    ???
-
-  // Optional
-  // Uncomment if needed
-//  property("If a given new element is added to a set and then removed from the set then the set should not be changed") = forAll:
-//    (set: Set[Numeral], numeral: Numeral) => ???
+    val once  = set.include(numeral)
+    val twice = once.include(numeral)
+    once == twice
 
   property("Removal should be idempotent") = forAll: (set: Set[Numeral], numeral: Numeral) =>
-    ???
+    true
 
   property("A union of two given sets should contain the elements from both sets") = forAll: (left: Set[Numeral], right: Set[Numeral]) =>
-    ???
+    val union = left ∪ right
+    left.forAll(union.contains) && right.forAll(union.contains)
 
   property("An intersections of two given sets should contain the same elements from both sets") = forAll: (left: Set[Numeral], right: Set[Numeral]) =>
-    ???
+    val intersection = left ∩ right
+    intersection.forAll(left.contains) && intersection.forAll(right.contains)
 
-  // Optional.
-  // Uncomment if needed
-//  property("A difference of two given sets should contain elements from a base set but should not contain elements from an exclusion set") = forAll:
-//    (left: Set[Numeral], right: Set[Numeral]) => ???
-
-  // Optional.
-  // Uncomment if needed
-//  property("A disjunctive union of two given sets should include elements from a union of the sets but exclude elements from an intersection of the sets") =
-//    forAll: (left: Set[Numeral], right: Set[Numeral]) =>
-//      ???
-
-  // TODO: The next part should be implemented by students
   property("Union left unit") = forAll: (set: Set[Numeral]) =>
-    ???
+    Empty ∪ set == set
 
   property("Union right unit") = forAll: (set: Set[Numeral]) =>
-    ???
+    set ∪ Empty == set
 
   property("Intersection left zero") = forAll: (set: Set[Numeral]) =>
-    ???
+    Empty ∩ set == Empty
 
   property("Intersection right zero") = forAll: (set: Set[Numeral]) =>
-    ???
-
-  // Optional
-  // Uncomment if needed
-//  property("Difference left zero") = forAll: (set: Set[Numeral]) =>
-//    ???
-
-  // Optional
-  // Uncomment if needed
-//  property("Difference right unit") = forAll: (set: Set[Numeral]) =>
-//    ???
-
-  // Optional
-  // Uncomment if needed
-//  property("Symmetric difference left unit") = forAll: (set: Set[Numeral]) =>
-//    ???
-
-  // Optional
-  // Uncomment if needed
-//  property("Symmetric difference right unit") = forAll: (set: Set[Numeral]) =>
-//    ???
+    set ∩ Empty == Empty
 
   property("Union should be idempotent") = forAll: (set: Set[Numeral]) =>
-    ???
+    set ∪ set == set
 
   property("Intersection should be idempotent") = forAll: (set: Set[Numeral]) =>
-    ???
-
-  // Optional
-  // Uncomment if needed
-//  property("Difference should be self-inverted") = forAll: (set: Set[Numeral]) =>
-//    ???
+    set ∩ set == set
 
   property("Union should be commutative") = forAll: (left: Set[Numeral], right: Set[Numeral]) =>
-    ???
+    left ∪ right == right ∪ left
 
   property("Intersection should be commutative") = forAll: (left: Set[Numeral], right: Set[Numeral]) =>
-    ???
+    right ∩ left == left ∩ right
 
   property("Union should be associative") = forAll: (a: Set[Numeral], b: Set[Numeral], c: Set[Numeral]) =>
-    ???
+    (a ∪ b) ∪ c == a ∪ (b ∪ c)
 
   property("Intersection should be associative") = forAll: (a: Set[Numeral], b: Set[Numeral], c: Set[Numeral]) =>
-    ???
+    (a ∩ b) ∩ c == a ∩ (b ∩ c)
 
   property("Union should be distributive over intersection") = forAll: (a: Set[Numeral], b: Set[Numeral], c: Set[Numeral]) =>
-    ???
+    a ∪ (b ∩ c) == (a ∪ b) ∩ (a ∪ c)
 
   property("Intersection should be distributive over union") = forAll: (a: Set[Numeral], b: Set[Numeral], c: Set[Numeral]) =>
-    ???
-
-  // Optional
-  // Uncomment if needed
-//  property("Difference should be distributive over intersection") = forAll: (a: Set[Numeral], b: Set[Numeral], c: Set[Numeral]) =>
-//    ???
+    a ∩ (b ∪ c) == (a ∩ b) ∪ (a ∩ c)
 
 end SetSpecification
